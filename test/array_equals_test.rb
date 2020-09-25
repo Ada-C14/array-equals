f55288087fa85a142a9ad128796022d51246ff54
Pauline Chane <pauline.chane@gmail.com>
2020-09-25 02:23:17 -0700

    Added array_equals implementation method to array_equals. Modified array_equals_test to remove deprecation warning without changing expected results.

--- test/array_equals_test.rb
+++ test/array_equals_test.rb
@@ -1,3 +1,19 @@
+# Pauline Chane (@PaulineChane on GitHub)
+# Ada Developers Academy C14
+# array_equals_test.rb
+# 09/29/2020
+
+# NOTE: WITHOUT changing the results of array_equals_test.rb,
+# made a minor edit to remove the deprecation warning:
+# anywhere with the following:
+# array_equals(array1,array2).must_equal [BOOLEAN]
+#
+# has now been converted to:
+# expect(array_equals(array1,array2)).must_equal [BOOLEAN]
+#
+# WITHOUT modifying the expected results.
+
+
 require 'minitest/autorun'
 require 'minitest/reporters'
 require_relative '../lib/array_equals'
@@ -8,21 +24,21 @@ describe "array equals" do
       array1 = [10, 20, 30, 40, 50, 60]
       array2 = [10, 20, 30, 40, 50, 60]
 
-      array_equals(array1, array2).must_equal true
+      expect(array_equals(array1, array2)).must_equal true
     end
 
     it "arrays not equal: first six elements are same" do
       array1 = [10, 20, 30, 40, 50, 60]
       array2 = [10, 20, 30, 40, 50, 60, 70]
 
-      array_equals(array1, array2).must_equal false
+      expect(array_equals(array1, array2)).must_equal false
     end
 
     it "arrays not equal: same count of elements" do
       array1 = [10, 20, 30, 40, 50, 60]
       array2 = [20, 3, 50, 10, 68, 74]
 
-      array_equals(array1, array2).must_equal false
+      expect(array_equals(array1, array2)).must_equal false
     end
   end
 
@@ -31,49 +47,49 @@ describe "array equals" do
       array1 = []
       array2 = []
 
-      array_equals(array1, array2).must_equal true
+      expect(array_equals(array1, array2)).must_equal true
     end
 
     it "only first array is empty: not equal" do
       array1 = []
       array2 = [50, 30]
 
-      array_equals(array1, array2).must_equal false
+      expect(array_equals(array1, array2)).must_equal false
     end
 
     it "only second array is empty: not equal" do
       array1 = [20]
       array2 = []
 
-      array_equals(array1, array2).must_equal false
+      expect(array_equals(array1, array2)).must_equal false
     end
 
     it "arrays are nil: equal" do
       array1 = nil
       array2 = nil
 
-      array_equals(array1, array2).must_equal true
+      expect(array_equals(array1, array2)).must_equal true
     end
 
     it "only first array is nil: not equal" do
       array1 = nil
       array2 = [10, 20]
 
-      array_equals(array1, array2).must_equal false
+      expect(array_equals(array1, array2)).must_equal false
     end
 
     it "only second array is nil: not equal" do
       array1 = [20]
       array2 = nil
 
-      array_equals(array1, array2).must_equal false
+      expect(array_equals(array1, array2)).must_equal false
     end
     
     it "will return false if the 1st elements are equal, but the 2nd aren't" do
       array1 = [20, 30]
       array2 = [20, 40]
       
-      array_equals(array1, array2).must_equal false
+      expect(array_equals(array1, array2)).must_equal false
     end
   end
 end
