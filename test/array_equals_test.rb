eb98c820d78ddef1926ab600afc1cdf5f0d6b686
Chris M <chris@adadevelopersacademy.org>
2020-09-25 12:04:14 -0700

    Move to `expect` syntax

--- test/array_equals_test.rb
+++ test/array_equals_test.rb
@@ -8,21 +8,21 @@ describe "array equals" do
       array1 = [10, 20, 30, 40, 50, 60]
       array2 = [10, 20, 30, 40, 50, 60]
 
-      array_equals(array1, array2).must_equal true
+      expect(array_equals(array1, array2)).must_equal true
     end
 
     it "arrays not equal: first six elements are same" do
       array1 = [10, 20, 30, 40, 50, 60]
       array2 = [10, 20, 30, 40, 50, 60, 70]
 
-      array_equals(array1, array2).must_equal false
+      expect(array_equals(array1, array2)).must_equal false
     end
 
     it "arrays not equal: same count of elements" do
       array1 = [10, 20, 30, 40, 50, 60]
       array2 = [20, 3, 50, 10, 68, 74]
 
-      array_equals(array1, array2).must_equal false
+      expect(array_equals(array1, array2)).must_equal false
     end
   end
 
@@ -31,49 +31,49 @@ describe "array equals" do
       array1 = []
       array2 = []
 
-      array_equals(array1, array2).must_equal true
+      expect(array_equals(array1, array2)).must_equal true
     end
 
     it "only first array is empty: not equal" do
       array1 = []
       array2 = [50, 30]
 
-      array_equals(array1, array2).must_equal false
+      expect(array_equals(array1, array2)).must_equal false
     end
 
     it "only second array is empty: not equal" do
       array1 = [20]
       array2 = []
 
-      array_equals(array1, array2).must_equal false
+      expect(array_equals(array1, array2)).must_equal false
     end
 
     it "arrays are nil: equal" do
       array1 = nil
       array2 = nil
 
-      array_equals(array1, array2).must_equal true
+      expect(array_equals(array1, array2)).must_equal true
     end
 
     it "only first array is nil: not equal" do
       array1 = nil
       array2 = [10, 20]
 
-      array_equals(array1, array2).must_equal false
+      expect(array_equals(array1, array2)).must_equal false
     end
 
     it "only second array is nil: not equal" do
       array1 = [20]
       array2 = nil
 
-      array_equals(array1, array2).must_equal false
+      expect(array_equals(array1, array2)).must_equal false
     end
     
     it "will return false if the 1st elements are equal, but the 2nd aren't" do
       array1 = [20, 30]
       array2 = [20, 40]
       
-      array_equals(array1, array2).must_equal false
+      expect(array_equals(array1, array2)).must_equal false
     end
   end
 end
