6e5c740051a52e2b8841d828047ada8ad644a373
Kimberly Vitug <kim_vitug05@yahoo.com>
2020-09-27 20:22:11 -0700

    refactor array_equals method

--- lib/array_equals.rb
+++ lib/array_equals.rb
@@ -1,19 +1,14 @@
 # Determines if the two input arrays have the same count of elements
 # and the same integer values in the same exact order
 def array_equals(array1, array2)
-  return true if array1.nil? && array2.nil?
-  return false if array1.nil? || array2.nil?
+  return true if (array1.nil? && array2.nil?)
+  return false if (array1.nil? || array2.nil?) || (array1.length != array2.length)
 
-  array1_to_hash = Hash.new(0)
-  array2_to_hash = Hash.new(0)
-
-  array1.each_with_index do |value, index|
-    array1_to_hash[value] = index
+  array1.length.times do |index|
+    if array1[index] != array2[index]
+      return false
+    end
   end
 
-  array2.each_with_index do |value, index|
-    array2_to_hash[value] = index
-  end
-
-  array1_to_hash <=> array2_to_hash ? (true) : (false)
+  return true
 end
