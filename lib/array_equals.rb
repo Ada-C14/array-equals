916d9936064c29ca41d354a33541c5d3ff41d57a
Stacy <lundquist.stacy@gmail.com>
2020-10-01 08:27:29 -0700

    fixing the syntax

--- lib/array_equals.rb
+++ lib/array_equals.rb
@@ -2,12 +2,10 @@
 # and the same integer values in the same exact order
 def array_equals(array1, array2)
   return true if array1.nil? && array2.nil?
-  return false if array1.nil? && !array2.nil? || !array1.nil? && array2.nil?
-  return false unless array1.length == array2.length
+  return false if (array1.nil? || array2.nil?) || (array1.length != array2.length)
 
   i = 0
-  l = array1.length - 1
-  while i < l
+  while i < array1.length - 1
     array1.each do
       return false unless array1[i] == array2[i]
 
