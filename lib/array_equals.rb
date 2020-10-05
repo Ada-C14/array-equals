bf488d1d0d25bf308dfb5186746e6b59c93f844e
Lina Do <l.do5147@gmail.com>
2020-10-04 22:16:58 -0700

    returns false if the array lengths don't match up

--- lib/array_equals.rb
+++ lib/array_equals.rb
@@ -1,5 +1,9 @@
 # Determines if the two input arrays have the same count of elements
 # and the same integer values in the same exact order
 def array_equals(array1, array2)
-  raise NotImplementedError
+
+  return false if array1.length != array2.length
+
+  
+
 end
