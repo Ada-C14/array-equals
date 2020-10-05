70463d12a5e7216613abb6711bf525ef05a7bf01
Lina Do <l.do5147@gmail.com>
2020-10-04 22:51:09 -0700

    changed the method to handle tests where one of the arrays is nil

--- lib/array_equals.rb
+++ lib/array_equals.rb
@@ -3,13 +3,12 @@
 def array_equals(array1, array2)
 
   if array1 == [] && array2 == [] || array1 == nil && array2 == nil
-    return true
-  elsif array1.length != array2.length
+  elsif (array1 == nil || array2 == nil) || (array1.length != array2.length)
     return false
-  end
-  
-  array1.each_with_index do |integer, index|
-    return false if integer != array2[index]
+  else
+    array1.each_with_index do |integer, index|
+      return false if integer != array2[index]
+    end
   end
 
   return true
