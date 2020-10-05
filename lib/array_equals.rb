cfdca4ae7a4a8007d9775a0e28732dd394caa4fa
Sandy <sandyvasquez9@gmail.com>
2020-10-05 14:07:08 -0700

    refactored arrays_equals

--- lib/array_equals.rb
+++ lib/array_equals.rb
@@ -1,31 +1,23 @@
 # Determines if the two input arrays have the same count of elements
 # and the same integer values in the same exact order
 def array_equals(array1, array2)
-  same_array = []
   if array1 == nil && array2 == nil
     return true
-  else
-
-    if array1 != nil && array2 == nil || array1 == nil && array2 != nil
-      return false
-    else
-    end
-
-    if(array1.length!=array2.length)
-      return false
-    else
-    end
-
-    array1.each_index do | ele|
-      if array1[ele] == array2[ele]
-        same_array.push(array1[ele])
-      end
-    end
-  end
-  if same_array.length == array2.length
-    return true
-  else
+  elsif array1 != nil && array2 == nil || array1 == nil && array2 != nil
     return false
   end
+
+  if(array1.length!=array2.length)
+    return false
+  end
+
+  array1.each_index do | ele|
+    if array1[ele] != array2[ele]
+      return false
+    end
+  end
+  return true
 end
+
+
 
