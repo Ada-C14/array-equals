6fe3d79faf43b2cd094a6e49a908c92ce241d1c4
Taylor Tompkins <taylormililani@gmail.com>
2020-10-06 12:31:11 -0700

    this'll do

--- lib/array_equals.rb
+++ lib/array_equals.rb
@@ -1,15 +1,18 @@
-# Determines if the two input arrays have the same count of elements
+# Determines if the two input arrays have the same count of elecdments
 # and the same integer values in the same exact order
 def array_equals(array1, array2)
+
   if array1.length != array2.length
     return false
   end
+
   i = 0
-  array1.length.times do
-    if array1[i] != array2[i]
+  array1.each do |element|
+    if element != array2[i]
       return false
     end
     i += 1
   end
+
   return true
 end
