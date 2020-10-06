0652c434460bd4dfdfeb959460488659090f1ee1
India Kato <india.kato@gmail.com>
2020-10-06 08:21:53 -0700

    fixed edge cases for nil arrays

--- lib/array_equals.rb
+++ lib/array_equals.rb
@@ -2,24 +2,29 @@
 # and the same integer values in the same exact order
 def array_equals(array1, array2)
   index = 0
-  if array1 == nil || array2 == nil
-    return false
-  elsif (array1 == [] && array2 == [])
+  unless array1 == nil || array2 == nil
+    while index < array1.length || index < array2.length
+      if (array1 == [] && array2 == [])
+        return true
+      elsif (array1 == [] && array2 != [])
+        return false
+      elsif (array2 == [] && array1 != [])
+        return false
+      elsif array1[index] == array2[index] && array1.length == array2.length
+        index += 1
+      else
+        return false
+      end
+    end
     return true
-  elsif (array1 == [] && array2 != [])
-    return false
-  elsif (array2 == [] && array1 != [])
-    return false
-  elsif array1 == nil && array2 == nil
+  end
+
+  if array1 == nil && array2 == nil
     return true
-  elsif array1 == nil && array2 != nil
+  elsif array1 == nil
     return false
-  elsif array2 == nil && array1 != nil
-    return false
-  elsif array1[index] == array2[index] && array1.length == array2.length
-    index += 1
-  else
+  elsif array2 == nil
     return false
   end
-  return true
+
 end
