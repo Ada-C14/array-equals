0807f1e977f583bb822ccea33759129d582d0095
Rachael Gomez <gomezrc1220@gmail.com>
2020-10-06 17:11:04 -0700

    all tests pass

--- lib/array_equals.rb
+++ lib/array_equals.rb
@@ -1,5 +1,36 @@
+# frozen_string_literal: true
+
 # Determines if the two input arrays have the same count of elements
 # and the same integer values in the same exact order
 def array_equals(array1, array2)
-  raise NotImplementedError
-end
+  # if array1.nil? && array2.nil?
+  #   return true
+  # else array1.nil? || array2.nil?
+  # return false
+  # end
+
+  # array1.each_with_index do |x, y |
+  #   array2.each_with_index do |z, w|
+  #     if array1[x] == array2[z]
+  #       return true
+  #     end
+  #   end
+  # end
+
+
+
+  if array1.nil? && array2.nil?
+      return true
+  elsif array1.nil? || array2.nil?
+    return false
+  elsif array1.length != array2.length
+    return false
+  elsif
+       array1.each_with_index do |item, index|
+           if array1[index] != array2[index]
+             return false
+           end
+         end
+  end
+  return true
+  end
